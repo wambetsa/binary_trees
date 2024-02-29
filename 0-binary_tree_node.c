f11f651f2def027e8ac2ff6061427ce6df7bb470
wambetsa emmanuel <wambetsae@gmail.com>
2024-02-29 23:24:59 +0300

    format binary_tree_node based on betty standards

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,10 +8,11 @@
  * Return: if an error occurs - NULL
  *         Otherwise - a pointer to the new node
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+    binary_tree_t *new_node;
+    
+    new_node = malloc(sizeof(binary_tree_t));
     if (new_node == NULL)
     {
         return (NULL);
