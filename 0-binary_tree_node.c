c087e6a87ea70fb2ad643b5e29990cf741aaa6ab
wambetsa emmanuel <wambetsae@gmail.com>
2024-02-29 23:31:06 +0300

    format binary_tree_node based on betty standards

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,24 +4,24 @@
  * binary_tree_node - Creates a binary tree node
  * @parent: A pointer to the parent node of the node to create
  * @value: Is the value to put in the new node
- * 
- * Return: if an error occurs - NULL
- *         Otherwise - a pointer to the new node
+ *
+ * Return: if an error occurs - NULL Otherwise - a pointer to the new node
  */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new_node;
-    
+
     new_node = malloc(sizeof(binary_tree_t));
     if (new_node == NULL)
     {
         return (NULL);
     }
-    
+
     new_node->n = value;
     new_node->parent = parent;
     new_node->left = NULL;
     new_node->right = NULL;
-    
+
     return (new_node);
 }
